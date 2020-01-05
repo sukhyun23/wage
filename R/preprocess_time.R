7b9518a609d4768219fd3dce39ebda995ad09baf
sukhyun23 <shkwon23@gmail.com>
2020-01-05 19:01:59 +0900

    version_0001

--- R/preprocess_time.R
+++ R/preprocess_time.R
@@ -1,75 +1,61 @@
 preprocess_time <- function(data) {
-  library(data.table)
-  data <- data.table::data.table(data)
+  agg <- function(x, FUN, group, name) {
+    result <- aggregate(
+      x = x,
+      by = group,
+      FUN
+    )
+    names(result) <- c('id', '이름', name)
+    return(result)
+  }
   
   # work day
-  d9 <- data[
-    !data$day %in% c('일요일', '토요일'), 
-    .(근무일 = length(unique(date))), 
-    by = .(id, 이름)
-    ]
+  idx <- !data$day %in% c('일요일', '토요일')
+  x <- data$date[idx]
+  group <- list(data$id[idx], data$이름[idx])
+  d9 <- agg(x, function(x) length(unique(x)), group, '근무일')
   
   # 기본 근로 시간 : 평일 8시간 이내
   # 평일 주간 일반
-  d1 <- data[
-    holiday == F, 
-    .(week_day_norm = sum(base_hour_day, na.rm = T)), 
-    by = .(id, 이름)
-    ]
+  idx <- !data$holiday
+  x <- data$base_hour_day[idx]
+  group <- list(data$id[idx], data$이름[idx])
+  d1 <- agg(x, sum, group, 'week_day_norm')
+  
+  
   # 평일 야간 일반
-  d3 <- data[
-    holiday == F, 
-    .(week_night_norm = sum(base_hour_night, na.rm = T)), 
-    by = .(id, 이름)
-    ]
+  x <- data$base_hour_night[idx]
+  d3 <- agg(x, sum, group, 'week_night_norm')
   
   # 연장 근로 시간 : 평일 8시간 초과
   # 평일 주간 연장
-  d2 <- data[
-    # type == '주간' & holiday == F, 
-    holiday == F,
-    .(week_day_over = sum(over_hour_day, na.rm = T)), 
-    by = .(id, 이름)
-    ]
+  x <- data$over_hour_day[idx]
+  d2 <- agg(x, sum, group, 'week_day_over')
+  
+  
   # 평일 야간 연장
-  d4 <- data[
-    # type == '야간' & holiday == F, 
-    holiday == F,
-    .(week_night_over = sum(over_hour_night, na.rm = T)), 
-    by = .(id, 이름)
-    ]
+  x <- data$over_hour_night[idx]
+  d4 <- agg(x, sum, group, 'week_night_over')
   
   # 기본 근로 시간 (휴일) : 8시간 이내
   # 휴일 주간 일반
-  d5 <- data[
-    # type == '주간' & holiday == T, 
-    holiday == T, 
-    .(holi_day_norm = sum(base_hour_day, na.rm = T)), 
-    by = .(id, 이름)
-    ]
+  idx <- data$holiday
+  x <- data$base_hour_day[idx]
+  group <- list(data$id[idx], data$이름[idx])
+  d5 <- agg(x, sum, group, 'holi_day_norm')
+  
   # 휴일 야간 일반
-  d7 <- data[
-    # type == '야간' & holiday == T,
-    holiday == T, 
-    .(holi_night_norm = sum(base_hour_night, na.rm = T)),
-    by = .(id, 이름)
-    ]
+  x <- data$base_hour_night[idx]
+  d7 <- agg(x, sum, group, 'holi_night_norm')
   
   # 연장 근로 시간 (휴일) : 8시간 초과
   # 휴일 주간 연장
-  d6 <- data[
-    # type == '주간' & holiday == T, 
-    holiday == T, 
-    .(holi_day_over = sum(over_hour_day, na.rm = T)), 
-    by = .(id, 이름)
-    ]
+  x <- data$over_hour_day[idx]
+  d6 <- agg(x, sum, group, 'holi_day_over')
+  
   # 휴일 야간 연장
-  d8 <- data[
-    # type == '야간' & holiday == T, 
-    holiday == T, 
-    .(holi_night_over = sum(over_hour_night, na.rm = T)),
-    by = .(id, 이름)
-    ]
+  x <- data$over_hour_night[idx]
+  d8 <- agg(x, sum, group, 'holi_night_over')
   
   result <- Reduce(
     function(...) merge(..., by = c('id', '이름'), all = T), 
@@ -87,9 +73,9 @@ preprocess_time <- function(data) {
   result$휴일연장시간 <- result$holi_day_over + result$holi_night_over
   result$야간근로시간 <- result$week_night_norm + result$holi_night_norm
   
-  result <- result[, .(
-    id, 이름, 근무일, 기본근로시간, 연장근로시간,
-    휴일근로시간, 휴일연장시간, 야간근로시간
+  result <- result[, c(
+    'id', '이름', '근무일', '기본근로시간', '연장근로시간',
+    '휴일근로시간', '휴일연장시간', '야간근로시간'
   )]
   
   return(result)
